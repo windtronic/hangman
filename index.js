d3150a52e0768342289ea26a94c66b35625327be
Toni <windtronic89@gmail.com>
2023-02-06 08:54:19 -0700

    working in JS

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-const container = document.querySelector(".keyboard-container")  // class selector from HTML (".className")
+const keyboard = document.querySelector(".keyboard")
+const container = document.querySelectorAll(".letter")  // class selector from HTML (".className")
 const displayAnswer = document.getElementById("displayAnswer") // ID selector from HTML 
 const resetButton = document.getElementById("reset") // ID selector from HTML
-const emptyLetter = document.getElementById("empty-letter-container") 
+const emptyLetter = document.getElementById("empty-letters") 
 // ID selector from HTML
 // another way to grab ID selector from HTML(querySelector needs an ID of main) example --> <p id="description class="main> "Education.launchcode.org"
 
@@ -15,6 +16,7 @@ let alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", // a
 for (let i = 0; i < alphabet.length; i++) {
     console.log(alphabet[i].length)
 }
+console.log(alphabet)
 
 let words = [                       
     "dream", "matrix", "Onyx",
@@ -24,14 +26,18 @@ let words = [
     "pixel", "puppy", "snazzy"
 ]
 
+
 let word = words[Math.floor(Math.random() * words.length)]  // pick random word from the array of words //W3 Schools
 
 let wordArray = []
 for (let i = 0; i < word.length; i++) {   // for loop to start at 0 and goes up by 1 and will not surpass the length of words.
     wordArray[i] = "_"  // to end the for loop at the random generated word length. 
 }
-
+console.log(word)
 let remainder = word.length // will give the length of the word when generated.
+
+
+// console.log(remainder)
 
 
 function playerPick() {    // function provides availability to use over and over again instead of writing it out multiple times. (W3 Schools)
