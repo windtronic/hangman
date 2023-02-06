8d576e9d857735b0550cbb60a023f206687970fa
Toni <windtronic89@gmail.com>
2023-02-06 13:29:52 -0700

    progress

--- index.js
+++ index.js
@@ -1,22 +1,20 @@
 const keyboard = document.querySelector(".keyboard")
-const container = document.querySelectorAll(".letter")  // class selector from HTML (".className")
+const letters = document.querySelectorAll(".letter")  // class selector from HTML (".className")
 const displayAnswer = document.getElementById("displayAnswer") // ID selector from HTML 
 const resetButton = document.getElementById("reset") // ID selector from HTML
 const emptyLetter = document.getElementById("empty-letters") 
-// ID selector from HTML
-// another way to grab ID selector from HTML(querySelector needs an ID of main) example --> <p id="description class="main> "Education.launchcode.org"
+  
+                                                                                                  // another way to grab ID selector from HTML(querySelector needs an ID of main) example --> <p id="description class="main> "Education.launchcode.org"
+// the letters chosen will be single letters from the html below.
+  
+
+letters.forEach((letter)=> {
+    letter.addEventListener("click", (letters)=> {
+        console.log(letter)
+    })
+})
 
 
- // the letters chosen will be single letters from the chosen array below.
-
-let alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", // array of letters in the alphabet
-"m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-
-
-for (let i = 0; i < alphabet.length; i++) {
-    console.log(alphabet[i].length)
-}
-console.log(alphabet)
 
 let words = [                       
     "dream", "matrix", "Onyx",
@@ -25,20 +23,30 @@ let words = [
     "topaz", "lucky", "avenue",
     "pixel", "puppy", "snazzy"
 ]
+console.log(words)
 
 
-let word = words[Math.floor(Math.random() * words.length)]  // pick random word from the array of words //W3 Schools
+let randomWord = words[Math.floor(Math.random() * words.length)]  // pick random word from the array of words //W3 Schools
+console.log(randomWord)
 
-let wordArray = []
-for (let i = 0; i < word.length; i++) {   // for loop to start at 0 and goes up by 1 and will not surpass the length of words.
-    wordArray[i] = "_"  // to end the for loop at the random generated word length. 
-}
-console.log(word)
-let remainder = word.length // will give the length of the word when generated.
+let choice = "";
+let count = 6;
+let mistakes = 0;
+let guessed = [];
 
 
-// console.log(remainder)
+
+
+
+
+
+
 
 
 function playerPick() {    // function provides availability to use over and over again instead of writing it out multiple times. (W3 Schools)
 }
+
+//Restart Game // and add an Event listener
+resetButton.addEventListener("click", () => {
+    //need the conditions in place to reset //
+})
